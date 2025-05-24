91a44db83133e4ffb741d5005b581c8bb58c2d28
laurynasra <ragauskas.laurynas@gmail.com>
2025-05-24 19:36:55 +0300

    Working wise export parser.

--- expense.go
+++ expense.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/sfomuseum/go-csvdict/v2"
+	"github.com/urfave/cli/v3"
 )
 
 type Expense struct {
@@ -15,11 +19,13 @@ type Expense struct {
 	Date        time.Time
 	Provider    string
 	Category    string
+	Matched     bool
 }
 
 type ExpenseCategory struct {
 	Amount   float64
 	Category string
+	Expenses []*Expense
 	Matchers []string
 }
 
@@ -57,12 +63,82 @@ func MapWiseExpense(row map[string]string) (*Expense, error) {
 	}, nil
 }
 
-func parseAndAggregate(fileName string) error {
+func parseAndAggregate(fileName string) (*ExpenseCategories, []*Expense, error) {
 	expenseCategories := &ExpenseCategories{}
+
+	expenseCategories.AddCategory(&ExpenseCategory{
+		Amount:   0,
+		Category: "IGNORE",
+		Matchers: []string{
+			"laurynas ragauskas", "nexo", "apple", "cashback", "converted", "laurynas", "ragauskas",
+		},
+	})
+
 	expenseCategories.AddCategory(&ExpenseCategory{
 		Amount:   0,
 		Category: "Food",
-		Matchers: []string{"maxima", "lidl"},
+		Matchers: []string{
+			"maxima", "lidl", "vaisiai", "darzov", "iki ", "rimi", "mangas",
+			"mangu", "turgelis",
+		},
+	})
+
+	expenseCategories.AddCategory(&ExpenseCategory{
+		Amount:   0,
+		Category: "Takeaway",
+		Matchers: []string{
+			"restoranas", "tores", "bravoras", "charlie pizza",
+			"narvesen", "caffeine", "crustum", "sokoladine", "heydekrug",
+			"coffee", "marinara", "mcdonalds", "sushi", "bolt",
+		},
+	})
+
+	expenseCategories.AddCategory(&ExpenseCategory{
+		Amount:   0,
+		Category: "Transport",
+		Matchers: []string{
+			"express pro", "circle k", "orlen", "p8",
+		},
+	})
+
+	expenseCategories.AddCategory(&ExpenseCategory{
+		Amount:   0,
+		Category: "Home",
+		Matchers: []string{
+			"knygos", "geliu parduotuve", "geles",
+		},
+	})
+
+	expenseCategories.AddCategory(&ExpenseCategory{
+		Amount:   0,
+		Category: "Health",
+		Matchers: []string{
+			"benu", "klinika", "youdek",
+		},
+	})
+
+	expenseCategories.AddCategory(&ExpenseCategory{
+		Amount:   0,
+		Category: "Vacation",
+		Matchers: []string{
+			"antalya",
+		},
+	})
+
+	expenseCategories.AddCategory(&ExpenseCategory{
+		Amount:   0,
+		Category: "Other",
+		Matchers: []string{
+			"royal smoke",
+		},
+	})
+
+	expenseCategories.AddCategory(&ExpenseCategory{
+		Amount:   0,
+		Category: "Clothes",
+		Matchers: []string{
+			"viln nordica sd", // Sports Direct Nordica
+		},
 	})
 
 	parsedExpenses, nil := parseExpenses(fileName)
@@ -70,52 +146,77 @@ func parseAndAggregate(fileName string) error {
 		for _, expenseCategory := range expenseCategories.categories {
 			if expenseCategory.Match(strings.ToLower(expense.Description)) {
 				expenseCategory.Amount += expense.Amount
+				expense.Matched = true
+				expenseCategory.Expenses = append(expenseCategory.Expenses, expense)
 				break //stop matching further categories
 			}
 		}
 	}
 	fmt.Println(expenseCategories)
-	return nil
+	unmatchedExpenses := []*Expense{}
+	for _, expense := range parsedExpenses {
+		if !expense.Matched {
+			unmatchedExpenses = append(unmatchedExpenses, expense)
+		}
+	}
+	return expenseCategories, parsedExpenses, nil
 }
 
 func main() {
-	parseAndAggregate("wise-05.csv")
-	// supportedProviders := []string{"Wise", "SEB", "Revolut"}
-	// cmd := &cli.Command{
-	// 	Commands: []*cli.Command{
-	// 		{
-	// 			Name:  "list-supported",
-	// 			Usage: "Lists supported providers",
-	// 			Action: func(ctx context.Context, cmd *cli.Command) error {
-	// 				fmt.Println("Supported providers:", supportedProviders)
-	// 				return nil
-	// 			},
-	// 		},
-	// 		{
-	// 			Name:  "parse-expenses",
-	// 			Usage: "Parses expense report for given provider. Prints out aggregated and categorized expenses",
-	// 			Flags: []cli.Flag{
-	// 				&cli.StringFlag{
-	// 					Name:     "provider",
-	// 					Usage:    "Provider to parse expenses for",
-	// 					Required: true,
-	// 				},
-	// 				&cli.StringFlag{
-	// 					Name:     "file",
-	// 					Usage:    "File to parse expenses from",
-	// 					Required: true,
-	// 				},
-	// 			},
-	// 			Action: func(ctx context.Context, cmd *cli.Command) error {
-	// 				fileName := cmd.String("file")
-	// 				return parseAndAggregate(fileName)
-	// 			},
-	// 		},
-	// 	},
-	// }
-	// if err := cmd.Run(context.Background(), os.Args); err != nil {
-	// 	log.Fatal(err)
-	// }
+	// parseAndAggregate("statement_11267423_EUR_2025-05-01_2025-05-24.csv")
+	supportedProviders := []string{"Wise", "SEB", "Revolut"}
+	cmd := &cli.Command{
+		Commands: []*cli.Command{
+			{
+				Name:  "list-supported",
+				Usage: "Lists supported providers",
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					fmt.Println("Supported providers:", supportedProviders)
+					return nil
+				},
+			},
+			{
+				Name:  "parse-expenses",
+				Usage: "Parses expense report for given provider. Prints out aggregated and categorized expenses",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "provider",
+						Usage:    "Provider to parse expenses for",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "file",
+						Usage:    "File to parse expenses from",
+						Required: true,
+					},
+				},
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					fileName := cmd.String("file")
+					expenseCategories, parsedExpenses, err := parseAndAggregate(fileName)
+					if err != nil {
+						return err
+					}
+					fmt.Println("Grouped categories:")
+					for _, category := range expenseCategories.categories {
+						fmt.Printf("%s: %f\n", category.Category, category.Amount)
+						for _, expense := range category.Expenses {
+							fmt.Printf("\t%s: %f\n", expense.Description, expense.Amount)
+						}
+					}
+					fmt.Println("Unmatched expenses:")
+					for _, expense := range parsedExpenses {
+						if !expense.Matched {
+							fmt.Printf("%s: %f\n", expense.Description, expense.Amount)
+						}
+					}
+					return nil
+				},
+			},
+		},
+	}
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func parseExpenses(fileName string) ([]*Expense, error) {
--- go.mod
+++ go.mod
@@ -6,3 +6,9 @@ require (
 	github.com/sfomuseum/go-csvdict/v2 v2.0.1
 	github.com/urfave/cli/v3 v3.3.2
 )
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli v1.22.16 // indirect
+)
